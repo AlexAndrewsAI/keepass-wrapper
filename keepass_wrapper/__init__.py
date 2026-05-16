b6ee71b02776ba9926ba1215f5a54768183ff7e8
AlexAndrewsAI <alex.andrews.ai@protonmail.com>
2026-05-15 21:21:23 -0400

    version bump

--- keepass_wrapper/__init__.py
+++ keepass_wrapper/__init__.py
@@ -2,5 +2,5 @@ from keepass_wrapper.config import DEFAULT_TEST_DATABASE, Config
 from keepass_wrapper.entry import KeePassEntry
 from keepass_wrapper.keepass import KeePass
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["KeePass", "KeePassEntry", "Config", "DEFAULT_TEST_DATABASE"]
